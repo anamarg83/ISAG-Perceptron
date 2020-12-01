f66cbf662d71cd95363b65ae72028f55470f5865
Ana Margarida Ferreira <anamarg83@gmail.com>
2020-12-01 17:39:08 +0000

    Definição de funções para organizar o código (createDataset, createClasses, createWeights, geral, createGraph, userInteractions)

--- perceptron.py
+++ perceptron.py
@@ -6,8 +6,18 @@
     # função calculo da previsão 
 # função de criação do gráfico 
 
+# population = [[0.3,0.7,0.1],[-0.6,0.3,0.2],[-0.1,-0.8,0.3],[0.1,-0.45,0.4]]
+# classes = [1,0,0,1]
+# weights = [0.8,-0.5,-0.1]
+
+# fazer comentários no código
+# erro 
+# gráfico
+# input nao funciona no visual code studio
+
 # Import Libraries
-from random import choices
+from random import choices, choice
+import random 
 
 # Criação da população 
 def createDataset(nrows, nvariables): 
@@ -21,27 +31,18 @@ def createClasses(nrows):
 def createWeights(nvariables): 
     return [n/10 for n in choices(range(1,5),k=nvariables)]
 
-#final_values = [1,0,1,0,1]
-#weights = [9,5]
-#population = createDataset(5,2)
-
-nr_interactions = []
-error_value = []
-prob_classe = []
-
 def userInteractions(): 
     print("Qual o número de iterações?")
-    nr_interactions = input()
+    nr_interactions = int(input())
     
     print("Qual a mínima alteração do erro?")
-    error_value = input()
+    error_value = float(input())
 
     print("Qual a probabilidade para definir classes?")
-    prob_classe = input()
-
-population = [[0.3,0.7,0.1],[-0.6,0.3,0.2],[-0.1,-0.8,0.3],[0.1,-0.45,0.4]]
-classes = [1,0,0,1]
-weights = [0.8,-0.5,-0.1]
+    prob_classe = float(input())
+    
+    return [nr_interactions, error_value, prob_classe]
+    
 def lossFunction(sum_per_observation, prob_classe): 
     if(sum_per_observation > prob_classe): 
         return 1 
@@ -72,13 +73,30 @@ def basic_perceptron(nr_iterations, p_classe):
 def perceptronAccurancy(original, predictions): 
     acc = str(1-sum([o - p for o, p in zip(original,predictions)])/len(original))
     return f"A taxa de precisão do nosso algoritmo Perceptron é: {acc}"
-    
-population = createDataset(5,2)
-classes = createClasses(5)
-weights = createWeights(2)
-print(population)
-print(classes)
-print(weights)
-predictions = basic_perceptron(5,0.5)
-predictions
-perceptronAccurancy(classes, predictions)
+
+def createGraph()
+    print("1")
+
+def geral(): 
+    #user inputs
+    inputs = userInteractions() 
+    nr_iterations = inputs[0]
+    error_value = inputs[1]
+    prob_classe = inputs[2]
+
+    #dataset creation
+    population = createDataset(10,2)
+    classes = createClasses(10)
+    weights = createWeights(2)
+
+    #print(population)
+    #print(classes)
+    #print(weights)
+
+    #perceptron
+    predictions = basic_perceptron(nr_iterations,prob_classe)
+    print(predictions)
+
+    #print(predictions)
+    acc = perceptronAccurancy(classes, predictions)
+    print(acc)
